5fb639f51901f33ca330b89d3eb13b0f68217de2
AleksanderMolak <aleksander.molak@gmail.com>
2018-08-28 15:08:39 +0200

    updates: scroll

--- site.js
+++ site.js
@@ -19,7 +19,7 @@ $.extend($.easing,
 
     $.fn.navScroller = function(options) {
         settings = $.extend({
-            scrollToOffset: 0, // previously: 170
+            scrollToOffset: 40, // previously: 170
             scrollSpeed: 800,
             activateParentNode: true,
         }, options );
